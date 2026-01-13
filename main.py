fa5ab9e62a5a6c1a0752029e0867e65d19ea9a9a
egramsdoescode <ethandoescode@gmail.com>
2026-01-13 08:06:53 -0600

    Create messages list to store conversation with gemini, add comments for clarity

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
 import os
+import argparse
+
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
+    # load secrets
     load_dotenv()
+
+    # Load in API key from secrets
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("api key not found")
 
+    # Gather user prompt
+    parser = argparse.ArgumentParser(description="AiChatBot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    # Generate client to comminicate with gemini
     client = genai.Client(api_key=api_key)
 
-    content = client.models.generate_content(
+    # Send and receive response from gemini
+    response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=messages,
     )
 
-    print(content.text)
+    # Print response metadata
+    if response.usage_metadata:
+        print(
+            f"""
+Prompt tokens: {response.usage_metadata.prompt_token_count}
+Response tokens: {response.usage_metadata.candidates_token_count}
+            """
+        )
+    else:
+        raise RuntimeError("usage_metadata not found")
+
+    # Print gemini's response
+    print(response.text)
 
 
 if __name__ == "__main__":
